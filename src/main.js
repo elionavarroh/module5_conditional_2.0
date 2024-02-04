9b3cbc3f80a07f5f1a8fb291dbf23478a04252e2
elionavarroh <elionavarroh@gmail.com>
2024-02-04 20:27:24 +0100

    add all the files of javascript

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import "./style.css";
+//Title of exercise
+console.log("Hello, this is te practice of Module 2, type of data!");
+
+//Info
+console.log({
+    Ticket: "120€",
+    Drinks: "18€",
+}
+);
+
+//Calculations
+console.log("Food without drinks");
+console.log(120-18); 
+console.log("Food for each person");
+console.log(102 / 6); 
