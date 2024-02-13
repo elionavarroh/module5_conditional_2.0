353fdaf3d781da21bcb2283b5c25f4b15dea4bc5
elionavarroh <elionavarroh@gmail.com>
2024-02-13 17:38:02 +0100

    add a new button and a new generation of number

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@
 // Variable que almacena la puntuación que lleve el usuario.
 let puntuacion = 0;
 
+//Bloqueo de botón 'y si no te hubieras plantado' para que salga bloqueado a inicio de partida.
+document.getElementById("queHabriaPasado").disabled = true;
+
 //Función para mostrar la puntuación.
 function muestraPuntuacion() {
   const elementoParrafo = document.getElementById("puntos");
-
+  console.log(puntuacion);
   if (elementoParrafo !== null && elementoParrafo !== undefined) {
     elementoParrafo.innerText = puntuacion;
   };
@@ -66,7 +69,7 @@ const damePuntoDeLaCarta = (carta) => {
   if (carta > 7) {
     return 0.5;
   } else {
-    carta;
+    return carta;
   };
 };
 
@@ -83,11 +86,11 @@ const setPuntuacion = (nuevoPunto) => {
 //Usamos un const seguido de una condición para gestionar la partida y ver si ha ganado o perdido.
 const gestionarPartida = () => {
   if (puntuacion === 7.5) {
-    ganarPartida();
+    return ganarPartida();
   }
   if (puntuacion > 7.5) {
-    perderPartida();
-  }
+    return perderPartida();
+  };
 };
 
 //Creamos una función que agrupa y llama a todas las funciones que se necesitan para pedir la carta. Esta es la función de pedir carta.
@@ -119,118 +122,106 @@ const perderPartida = () => {
 function mePlanto() {
   document.getElementById("pideCarta").disabled = true;
   document.getElementById("mePlanto").disabled = true;
+  document.getElementById("queHabriaPasado").disabled = false;
 };
 
 //Usamos una función para iniciar una nueva partida.
 function nuevaPartida() {
-  let puntuacion = 0;
+  puntuacion = 0;
   muestraPuntuacion();
+  gestionarPartida;
   document.getElementById("carta").src =
     "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg";
   document.getElementById("pideCarta").disabled = false;
   document.getElementById("mePlanto").disabled = false;
+  document.getElementById("queHabriaPasado").disabled = true;
 };
 
 //Eventos de los botones.
-document.getElementById("pideCarta").addEventListener("click", pideCarta);
-document.getElementById("mePlanto").addEventListener("click", mePlanto);
-document.getElementById("nuevaPartida").addEventListener("click", nuevaPartida);
+const botonPideCarta = document.getElementById("pideCarta");
 
+  if (botonPideCarta !== null && botonPideCarta !== undefined) {
+     botonPideCarta.addEventListener("click", pideCarta);
+};
 
+const botonMePlanto = document.getElementById("mePlanto");
 
+  if (botonMePlanto !== null && botonMePlanto !== undefined) {
+    botonMePlanto.addEventListener("click", mePlanto);
+};
 
-//ANTIGUO CÓDIGO
-// let puntuacion = 0;
+const botonNuevaPartida = document.getElementById("nuevaPartida");
 
-// // Muestra de la puntuación.
-// function muestraPuntuacion() {
-//   document.getElementById('puntos').innerText = puntuacion;
-// }
+  if (botonNuevaPartida !== null && botonNuevaPartida !== undefined) {
+    botonNuevaPartida.addEventListener("click", nuevaPartida);
+};
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   muestraPuntuacion();
-// });
+//PARTE EXTRA -> de 'qué habría pasado'
 
-// // Array con los valores posibles de las cartas. Los valores 8 y 9 no se incluyen, ya que no se usan en este juego,
-// // y el 10, 11 y 12 se representan con sota, caballo y rey, respectivamente (cada uno con valor 0.5).
-// const valoresPosibles = [1, 2, 3, 4, 5, 6, 7, 10, 11, 12];
+//Mostramos la puntuación
+function muestraPuntuacionNuevo() {
+  const elementoParrafo = document.getElementById("puntos");
+  console.log(puntuacion);
+  if (elementoParrafo !== null && elementoParrafo !== undefined) {
+    elementoParrafo.innerText = puntuacion;
+  };
+};
 
-// function dameCarta() {
-//   return valoresPosibles[Math.floor(Math.random() * valoresPosibles.length)];
-// }
+const dameUrlCartaNuevo = (cartaNuevo) => {
+  return cartasImagenes[cartaNuevo];
+ };
 
-// // Diccionario que asocia cada valor de carta con su imagen correspondiente.
-// const cartasImagenes = {
-//   1: 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/1_as-copas.jpg',
-//   2: 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/2_dos-copas.jpg',
-//   3: 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/3_tres-copas.jpg',
-//   4: 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/4_cuatro-copas.jpg',
-//   5: 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg',
-//   6: 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg',
-//   7: 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg',
-//   10: 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg',
-//   11: 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg',
-//   12: 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg',
-// };
+ function mostrarCartaNuevo (urlCarta) {
+  const elementoImagen = document.getElementById("carta");
 
-// function mostrarCarta(valor) {
-//   const src = cartasImagenes[valor];
-//   document.getElementById('carta').src = src;
-// }
+  if (elementoImagen !== null && elementoImagen !== undefined) {
+    elementoImagen.src = urlCarta;
+  };
+ };
 
-// function pideCarta() {
-//   const carta = dameCarta();
-//   mostrarCarta(carta);
-  
-//   // Si la carta elegida es sota, caballo o rey (10, 11 o 12, respectivamente), 
-//   // sumamos 0.5 en lugar de su valor nominal.
-//   if (carta === 10 || carta === 11 || carta === 12) {
-//     puntuacion += 0.5;
-//   } else {
-//     puntuacion += carta;
-//   }
-//   muestraPuntuacion();
+//Generamos un nuevo número aleatorio.
+const dameNumeroAleatorioNuevo = () => {
+  return Math.ceil(Math.random() * 10);
+};
 
-//   // Si la puntuación supera 7.5, el juego termina.
-//   if (puntuacion > 7.5) {
-//     alert("Game over ¡Prueba otra vez!");
-//     document.getElementById('pideCarta').disabled = true;
-//     document.getElementById('mePlanto').disabled = true;
-//   }
-// }
+//Damos el valor de la carta de nuevo.
+const dameValorCartaNuevo = (numeroAleatorioNuevo) => {
+  if (numeroAleatorioNuevo > 7) {
+    return numeroAleatorioNuevo + 2;
+  } else {
+    return numeroAleatorioNuevo;
+  };
+};
 
-// document.getElementById('pideCarta').addEventListener('click', pideCarta);
+//Volvemos a darle puntuación a la carta.
+const damePuntoDeLaCartaNuevo = (carta) => {
+  if (carta > 7) {
+    return 0.5;
+  } else {
+    return carta;
+  };
+};
 
-// function mePlanto() {
-//   // Deshabilitamos los botones de pedir carta y plantarse.
-//   document.getElementById('pideCarta').disabled = true;
-//   document.getElementById('mePlanto').disabled = true;
-  
-//   // A continuación, mostramos un mensaje en función de la puntuación.
-//   switch (puntuacion) {
-//     case 7.5:
-//       alert("¡Puntuación perfecta! ¡Has ganado!");
-//       break;
-//     case 6:
-//     case 7:
-//       alert("¡Nada mal! Te has quedado cerca");
-//       break;
-//     case 5:
-//       alert("¿Solo un 5?")
-//     default:
-//       alert("F, has sido muy conservador");
-//   }
-// }
+//Sumar puntos de nuevo.
+const sumarPuntosNuevo = (puntosParaSumar) => {
+  return puntuacion + puntosParaSumar;
+};
 
-// document.getElementById('mePlanto').addEventListener('click', mePlanto);
+//Función que agrupa y llama a las funciones nuevas
+function pideCartaNuevo() {
+  const numeroAleatorioNuevo = dameNumeroAleatorioNuevo();
+  const cartaNuevo = dameValorCartaNuevo(numeroAleatorioNuevo);
+  const puntoCarta = damePuntoDeLaCartaNuevo(cartaNuevo);
+  const puntosSumados = sumarPuntosNuevo(puntoCarta);
+  setPuntuacion(puntosSumados);
+  const urlCarta = dameUrlCarta(carta);
+  mostrarCartaNuevo(urlCarta);
+  muestraPuntuacionNuevo();
+};
 
-// function nuevaPartida() {
-//   // Reiniciamos la puntuación y mostramos la carta boca abajo.
-//   puntuacion = 0;
-//   muestraPuntuacion();
-//   document.getElementById('carta').src = 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg';
-//   document.getElementById('pideCarta').disabled = false;
-//   document.getElementById('mePlanto').disabled = false;
-// }
+//Eventos del botón nuevo
+const botonQueHabriaPasado = document.getElementById("queHabriaPasado");
 
-// document.getElementById('nuevaPartida').addEventListener('click', nuevaPartida);
+  if (botonQueHabriaPasado !== null && botonQueHabriaPasado !== undefined) {
+    botonQueHabriaPasado.addEventListener("click", pideCartaNuevo);
+};
